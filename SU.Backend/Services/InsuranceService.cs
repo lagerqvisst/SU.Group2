7cddad26853dee6add0e578e210116b5cef3d11e
AdamWadheden <153288071+AdamWadheden@users.noreply.github.com>
2024-09-27 17:23:05 +0200

    Added test code

--- SU.Backend/Services/InsuranceService.cs
+++ SU.Backend/Services/InsuranceService.cs
@@ -34,9 +34,11 @@ namespace SU.Backend.Services
         public async Task<(bool Success, string Message)> CreateTestInsurance()
         {
             //GIT TRAINING - REMOVE
-            _logger.LogInformation("SOME CODE CHANGES...");
+            _logger.LogInformation("SOME CODE CHANGES... :)");
 
             _logger.LogInformation("Creating insurance...");
+
+          // adding test comment
 
             try
             {
