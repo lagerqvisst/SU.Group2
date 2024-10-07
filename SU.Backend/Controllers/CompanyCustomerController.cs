e2205444cf76bed469bcd3a947245f6acdc210d3
Adam Ågestedt <adam.agestedt@gmail.com>
2024-10-07 11:38:02 +0200

    Added Controller class for CompanyCustomer and also added .NET version 6 in json after some building errors with the system using .NET 8

---
+++ SU.Backend/Controllers/CompanyCustomerController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Logging;
+using SU.Backend.Models.Customers;
+using SU.Backend.Services;
+using SU.Backend.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SU.Backend.Controllers
+{
+    public class CompanyCustomerController
+    {
+        private readonly ICompanyCustomerService _companyCustomerService;
+        private readonly ILogger<CompanyCustomerController> _logger;
+
+        public CompanyCustomerController(ICompanyCustomerService companyCustomerService, ILogger<CompanyCustomerController> logger)
+        {
+            _companyCustomerService = companyCustomerService;
+            _logger = logger;
+        }
+    }
+}
