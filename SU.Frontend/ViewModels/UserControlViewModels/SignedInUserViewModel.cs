67a25fa7fdce26722d81326dfd9d04bdf4310a9e
Adam Ågestedt <adam.agestedt@gmail.com>
2024-10-22 14:45:22 +0200

    SignedInUser panel for views

---
+++ SU.Frontend/ViewModels/UserControlViewModels/SignedInUserViewModel.cs
@@ -0,0 +1,50 @@
+﻿using SU.Backend.Models.Employees;
+using SU.Frontend.Helper;
+using SU.Frontend.Helper.User;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+
+public class SignedInUserViewModel : ObservableObject
+{
+    private readonly ILoggedInUserService _loggedInUserService;
+    public Employee _loggedInEmployee;
+
+    public SignedInUserViewModel(ILoggedInUserService loggedInUserService)
+    {
+        _loggedInUserService = loggedInUserService;
+        _loggedInEmployee = _loggedInUserService.LoggedInEmployee;
+    }
+
+
+    public string SignedInUserName
+    {
+        get => $"User logged in: {_loggedInEmployee.FirstName} {_loggedInEmployee.LastName}";
+    }
+
+    private string _signedInUserId;
+
+    public string SignedInUserId
+    {
+        get => $" User ID: {_loggedInEmployee.EmployeeId}";
+        set
+        {
+            _signedInUserId = value;
+            OnPropertyChanged();
+        }
+    }
+
+    private string _signedInUserRole;
+
+    public string SignedInUserRole
+    {
+        get => $" User role: {_loggedInEmployee.RoleAssignments.FirstOrDefault(x => x.Role != null).ToString()}";
+        set
+        {
+            _signedInUserRole = value;
+            OnPropertyChanged();
+        }
+    }
+
+
+}
