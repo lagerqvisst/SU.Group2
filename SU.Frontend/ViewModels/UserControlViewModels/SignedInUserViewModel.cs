d6f98b9e332fede94000f152acbf6213942f0cae
AdamWadheden <153288071+AdamWadheden@users.noreply.github.com>
2024-10-23 14:18:48 +0200

    Added comments to code we created yesterday

--- SU.Frontend/ViewModels/UserControlViewModels/SignedInUserViewModel.cs
+++ SU.Frontend/ViewModels/UserControlViewModels/SignedInUserViewModel.cs
@@ -8,27 +8,32 @@ using System.Linq;
 
 public class SignedInUserViewModel : ObservableObject
 {
+    // Provides information about the currently logged-in user
     private readonly ILoggedInUserService _loggedInUserService;
+
+    // The currently logged-in employee's information
     public Employee _loggedInEmployee;
 
+    // Constructor to initialize the logged-in employee 
     public SignedInUserViewModel(ILoggedInUserService loggedInUserService)
     {
         _loggedInUserService = loggedInUserService;
         _loggedInEmployee = _loggedInUserService.LoggedInEmployee;
     }
 
-
+    // Returns the logged-in user's name as a formatted string
     public string SignedInUserName
     {
         get => $"User logged in: {_loggedInEmployee.FirstName} {_loggedInEmployee.LastName}";
     }
 
+    // Returns the logged-in user's ID as a formatted string
     public string SignedInUserId
     {
         get => $"User ID: {_loggedInEmployee.EmployeeId}";
-  
     }
 
+    // Returns the logged-in user's highest percentage role via method, or 'None' if no role is assigned
     public string SignedInUserRole
     {
         get
@@ -37,6 +42,5 @@ public class SignedInUserViewModel : ObservableObject
             return role != null ? $"User role: {role}" : "User role: None";
         }
     }
-
-
 }
+
