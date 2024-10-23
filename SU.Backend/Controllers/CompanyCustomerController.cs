ba47ee5369cd733b62fe3a2fc46d578ffdd622d6
Kdaviid <Daviid-k95@hotmail.com>
2024-10-23 17:31:04 +0200

    UpdateCompanyCustomer method  in CompanyCustomerController,service , Iservice camelCase

--- SU.Backend/Controllers/CompanyCustomerController.cs
+++ SU.Backend/Controllers/CompanyCustomerController.cs
@@ -43,20 +43,20 @@ namespace SU.Backend.Controllers
             }
         }
 
-        public async Task<(bool Success, string Message)> UpdateCompanyCustomer(CompanyCustomer companyCustomer)
+        public async Task<(bool success, string message)> UpdateCompanyCustomer(CompanyCustomer companyCustomer)
         {
             _logger.LogInformation("Controller activated to update company customer...");
             var result = await _companyCustomerService.UpdateCompanyCustomer(companyCustomer);
 
-            if (result.Success)
+            if (result.success)
             {
-                _logger.LogInformation($"{result.Message}");
-                return (result.Success, result.Message);
+                _logger.LogInformation($"{result.message}");
+                return (result.success, result.message);
             }
             else
             {
-                _logger.LogWarning($"{result.Message}");
-                return (result.Success, result.Message);
+                _logger.LogWarning($"{result.message}");
+                return (result.success, result.message);
             }
         }
 
