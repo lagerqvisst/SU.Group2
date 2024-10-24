8c51e7836ff41f749a6817385a15c7363d4fd42e
Kdaviid <Daviid-k95@hotmail.com>
2024-10-24 23:04:43 +0200

    camelCase fixes in SellerStatistics.cs

--- SU.Backend/Models/Statistics/SellerStatistics.cs
+++ SU.Backend/Models/Statistics/SellerStatistics.cs
@@ -9,18 +9,18 @@ namespace SU.Backend.Models.Statistics
     public class SellerStatistics
     {
         // Basic information about the seller
-        public string SellerName { get; set; }
-        public string AgentNumber { get; set; }
+        public string sellerName { get; set; }
+        public string agentNumber { get; set; }
 
         // Monthly sales data
-        public List<MonthlySalesData> MonthlySales { get; set; } = new List<MonthlySalesData>();
+        public List<MonthlySalesData> monthlySales { get; set; } = new List<MonthlySalesData>();
 
         // Yearly totals and averages
-        public int TotalYearlySales { get; set; } // Sum of all insurances sold throughout the year
-        public double AverageMonthlySales { get; set; } // Average sales per month
+        public int totalYearlySales { get; set; } // Sum of all insurances sold throughout the year
+        public double averageMonthlySales { get; set; } // Average sales per month
 
         // Optional: Store the year for context
-        public int Year { get; set; }
+        public int year { get; set; }
     }
 
 }
