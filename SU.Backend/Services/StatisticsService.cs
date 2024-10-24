4dce8a26647747a6e27c95ccefa2072c0f46ce55
Kdaviid <Daviid-k95@hotmail.com>
2024-10-24 23:58:43 +0200

    PascalCase StatisticsService.cs

--- SU.Backend/Services/StatisticsService.cs
+++ SU.Backend/Services/StatisticsService.cs
@@ -52,8 +52,8 @@ namespace SU.Backend.Services
                     insurance => insurance.SellerId,
                     (seller, insurances) => new
                     {
-                        Seller = seller,
-                        MonthlySales = Enumerable.Range(1, 12).Select(month => new MonthlySalesData
+                        seller1 = seller,
+                        monthlySales = Enumerable.Range(1, 12).Select(month => new MonthlySalesData
                         {
                             Month = month,
                             InsuranceSalesCounts = insurances
@@ -64,11 +64,11 @@ namespace SU.Backend.Services
                     })
                     .Select(data => new SellerStatistics
                     {
-                        SellerName = data.Seller.FirstName + " " + data.Seller.LastName,
-                        AgentNumber = data.Seller.AgentNumber,
-                        MonthlySales = data.MonthlySales,
-                        TotalYearlySales = data.MonthlySales.Sum(m => m.TotalSales),
-                        AverageMonthlySales = data.MonthlySales.Average(m => m.TotalSales)
+                        SellerName = data.seller1.FirstName + " " + data.seller1.LastName,
+                        AgentNumber = data.seller1.AgentNumber,
+                        MonthlySales = data.monthlySales,
+                        TotalYearlySales = data.monthlySales.Sum(m => m.TotalSales),
+                        AverageMonthlySales = data.monthlySales.Average(m => m.TotalSales)
                     })
                     .ToList();
 
