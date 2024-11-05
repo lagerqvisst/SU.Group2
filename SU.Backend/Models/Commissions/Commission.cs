415ad1e799d0fc791e87a0030a86e251e232cb14
Alexander Lagerqvist <alexander.lagerqvist@hotmail.com>
2024-11-05 12:11:04 +0100

    fixed left join and new datepicker

--- SU.Backend/Models/Commissions/Commission.cs
+++ SU.Backend/Models/Commissions/Commission.cs
@@ -14,11 +14,11 @@ namespace SU.Backend.Models.Comissions
     {
         public string AgentNumber { get; set; }
         public string SellerName { get; set; }
+        public double CommissionAmount { get; set; }
         public string PersonalNumber { get; set; }
-
         public DateTime StartDate { get; set; } // Defines the search range for commissions
         public DateTime EndDate { get; set; }
-        public double CommissionAmount { get; set; }
+
 
         //This is based on the business rules that the commission is 12% of the premium acording to provided business documentation
         //Method is used in repository class when aggregating the commissions
--- SU.Backend/Services/CommissionService.cs
+++ SU.Backend/Services/CommissionService.cs
@@ -47,5 +47,6 @@ namespace SU.Backend.Services
             }
         }
 
+
     }
 }
